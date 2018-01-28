c225bcd62abef5e1413ffd0b54304b2297328b29
Uberto Barbini <uberto.gama@gmail.com>
2018-01-28 10:13:30 +0000

    isExpired test

--- src/main/java/com/gamasoft/offers/model/Offer.java
+++ src/main/java/com/gamasoft/offers/model/Offer.java
@@ -1,5 +1,6 @@
 package com.gamasoft.offers.model;
 
+import java.time.Duration;
 import java.time.Instant;
 import java.util.Date;
 
@@ -11,20 +12,22 @@ public class Offer {
     final String description;
     final String currency;
     final Double price;
-    final int validityInDays;
+    final Duration validity;
     final Date creationDate;
+    final Boolean isCancelled;
 
-    public Offer(String description, String currency, Double price, int validityInDays) {
+    public Offer(String description, String currency, Double price, Duration validity) {
         this.description = description;
         this.currency = currency;
         this.price = price;
-        this.validityInDays = validityInDays;
+        this.validity = validity;
         id = -1;
         creationDate = new Date();
+        isCancelled = false;
     }
 
     public Boolean isExpired(){
-        Instant expiry = creationDate.toInstant().plus(validityInDays, DAYS);
+        Instant expiry = creationDate.toInstant().plus(validity);
         return expiry.isBefore(Instant.now());
     }
 
--- src/test/java/com/gamasoft/offers/model/OfferTest.java
+++ src/test/java/com/gamasoft/offers/model/OfferTest.java
@@ -2,11 +2,26 @@ package com.gamasoft.offers.model;
 
 import org.junit.Test;
 
+import java.time.Duration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class OfferTest {
 
 
     @Test
-    public void expired() {
+    public void expireAfterValidityPassed() throws InterruptedException {
+
+        Offer offer = new Offer("very good stuff", "GBP", 123.45, Duration.ofMillis(100));
+
+        assertThat(offer.isExpired()).isFalse();
+
+        Thread.sleep(100);
+
+        assertThat(offer.isExpired()).isTrue();
 
     }
+
+
+
 }
